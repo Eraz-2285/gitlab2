c45c938e33e643e1a3e1230bc3c3a9c57b63bd62
Eraz <mohammediraz123@gmail.com>
2023-11-18 09:26:53 +0600

    cpp file added

---
+++ firstcd.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <conio.h>
+using namespace std;
+
+int main()
+{
+    cout << "Details :"<< endl;
+    cout<< "\n\tTamjidul Hasan Eraz\n" << "\tPremier University" << endl <<"\tProgram :B.Sc in CSE"<< endl <<"\tRoll : 2285"<<endl <<"\tBatch :40";
+    getch();
+}
+
+// 'endl' and '\n' is same things
