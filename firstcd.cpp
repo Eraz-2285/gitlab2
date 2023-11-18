086bc95b298e1fceb6aa5f060d1243334cb10586
Eraz-2285 <150497845+Eraz-2285@users.noreply.github.com>
2023-11-18 10:48:32 +0600

    Update firstcd.cpp

--- firstcd.cpp
+++ firstcd.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int main()
 {
     cout << "Details :"<< endl;
-    cout<< "\n\tTamjidul Hasan Eraz\n" << "\tPUC" << endl <<"\tProgram :B.Sc in CSE"<< endl <<"\tRoll : 2285"<<endl <<"\tBatch :40";
+    cout<< "\n\tTamjidul\n" << "\tPUC" << endl <<"\tProgram :B.Sc in CSE"<< endl <<"\tRoll : 2285"<<endl <<"\tBatch :40";
     getch();
 }
 
